413cadfef333cc76dd73ef4eb4228e84fbfba54b
Gabriel Lievano <jglievano@gmail.com>
2020-03-05 15:39:29 -0800

    Setup the base webpack config.

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -3,4 +3,39 @@ import webpack from 'webpack';
 import { dependencies as externals } from '../src/package.json';
 
 export default {
+  externals: [...Object.keys(externals || {})],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, '..', 'src'),
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: 'commonjs2'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: [path.join(__dirname, '..', 'src'), 'node_modules']
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production'
+    }),
+
+    new webpack.NamedModulesPlugin()
+  ]
 };
