8151d5412ffafc163ca068f8bfab06d356f566cd
Gabriel Lievano <jglievano@gmail.com>
2020-03-05 15:44:55 -0800

    Most initial dependencies sorted out.

--- configs/webpack.config.main.prod.babel.js
+++ configs/webpack.config.main.prod.babel.js
@@ -2,7 +2,48 @@ import path from 'path';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 import baseConfig from './webpack.config.base';
+import TerserPlugin from 'terser-webpack-plugin';
 
 export default merge.smart(baseConfig, {
+  devtool: process.env.DEBUG_PROD === 'true' ? 'source-map' : 'none',
 
+  mode: 'production',
+
+  target: 'electron-main',
+
+  entry: './src/main.dev.ts',
+
+  output: {
+    path: path.join(__dirname, '..'),
+    filename: './src/main.prod.js'
+  },
+
+  optimization: {
+    minimizer: process.env.E2E_BUILD
+      ? []
+      : [
+        new TerserPlugin({
+          parallel: true,
+          sourceMap: true,
+          cache: true
+        })
+      ]
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+      E2E_BUILD: false
+    })
+  ],
+
+  /**
+   * Disables webpack processing of __dirname and __filename.
+   * If you run the bundle in node.js it falls back to these values of node.js.
+   * https://github.com/webpack/webpack/issues/2010
+   */
+  node: {
+    __dirname: false,
+    __filename: false
+  }
 });
