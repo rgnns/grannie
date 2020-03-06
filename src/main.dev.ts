e20aea075813b0241f4a1e3516b5178a57f3cae1
Gabriel Lievano <jglievano@gmail.com>
2020-03-05 16:00:29 -0800

    Add the main code.

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -1,2 +1,32 @@
 /* eslint global-require: off, no-console: off */
 
+import { app, BrowserWindow } from 'electron';
+
+async function createWindow () {
+  if (process.env.NODE_ENV === 'development') {
+    //await installExtensions();
+  }
+
+  let win = new BrowserWindow({
+    show: false,
+    width: 1024,
+    height: 728,
+    webPreferences: { nodeIntegration: true }
+  });
+
+  win.loadURL(`file://${__dirname}/app.html`);			      
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
